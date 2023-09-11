bc34167b16a1763ccfb576c6fac356f957fdb40a
JulieneCarneiro <juliene.scarneiro@gmail.com>
2023-09-11 20:20:05 -0300

    feat: criação da model da entidade generos

---
+++ src/models/Generos.js
@@ -0,0 +1,18 @@
+class Generos{
+    constructor(livros, romance, terror, comedia, drama, aventura, autoajuda, comunicacao, academicos, lgbt, arte, programacao){
+        this.livros = livros 
+        this.romance = romance
+        this.terror = terror
+        this.comedia = comedia
+        this.drama = drama
+        this.aventura = aventura
+        this.autoajuda = autoajuda
+        this.comunicacao = comunicacao 
+        this.academicos = academicos
+        this.lgbt = lgbt 
+        this.arte = arte
+        this.programacao = programacao 
+}
+}
+
+export default Generos
