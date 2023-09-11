6ee99e0bb0e22053ac27e9c41387ffa17d151749
JulieneCarneiro <juliene.scarneiro@gmail.com>
2023-09-11 20:26:43 -0300

    feat: criação da model da entidade clientes

---
+++ src/models/Clientes.js
@@ -0,0 +1,11 @@
+class Clientes{
+    constructor(nome, email, telefone, endereco ){
+        this.nome = nome
+        this.email = email
+        this.telefone = telefone
+        this.endereco = endereco 
+        
+}
+}
+
+export default Clientes
