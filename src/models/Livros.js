d8a085e4fe4ba946aa5fafb7f4f3807de85ceaa6
JulieneCarneiro <juliene.scarneiro@gmail.com>
2023-09-11 20:01:55 -0300

    feat: criação da model da class livros

---
+++ src/models/Livros.js
@@ -0,0 +1,14 @@
+class Livros {
+    constructor(titulo, preco, autor, generos, num_pag, editora, idioma, capa){
+        this.titulo = titulo
+        this.preco = preco
+        this.autor = autor
+        this.generos = generos
+        this.num_pag = num_pag
+        this.editora = editora
+        this.idioma = idioma 
+        this.capa = capa
+    }
+}
+
+export default Livros
