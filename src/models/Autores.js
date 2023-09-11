4b15b36b3ecb08de7b4f8dba72a81d15d7cdb51c
JulieneCarneiro <juliene.scarneiro@gmail.com>
2023-09-11 20:13:44 -0300

    feat: criação da model da entidade autores

---
+++ src/models/Autores.js
@@ -0,0 +1,11 @@
+class Autores{
+    constructor(nome, pais, resumo, email, livros){
+        this.nome = nome
+        this.pais = pais
+        this.resumo = resumo
+        this.email = email
+        this.livros = livros 
+}
+}
+
+export default Autores
