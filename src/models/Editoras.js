480ffc7f8f7696c672f37cad03b0b530abb499b6
JulieneCarneiro <juliene.scarneiro@gmail.com>
2023-09-11 20:24:32 -0300

    feat: criação da model da entidade editoras

---
+++ src/models/Editoras.js
@@ -0,0 +1,11 @@
+class Editoras{
+    constructor(nome, cnpj, email, telefone, endereço){
+        this.nome = nome
+        this.cnpj = cnpj
+        this.email = email
+        this.telefone = telefone
+        this.endereco = endereco 
+}
+}
+
+export default Editoras
