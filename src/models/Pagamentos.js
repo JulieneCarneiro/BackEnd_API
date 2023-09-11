0b074cee6a35908641b45d5e7711f927a5e59877
JulieneCarneiro <juliene.scarneiro@gmail.com>
2023-09-11 20:28:49 -0300

    feat: criação da model da entidade pagamentos

---
+++ src/models/Pagamentos.js
@@ -0,0 +1,10 @@
+class Pagamentos{
+    constructor(pix, debito, credito, boleto){
+        this.pix = pix
+        this.debito = debito
+        this.credito = credito
+        this.boleto = boleto
+}
+}
+
+export default Pagamentos
