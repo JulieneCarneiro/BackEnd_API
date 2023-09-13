0de9572d2ed4208a66c35072803f0822468e52aa
Thzzao <thiago.santos1160@gmail.com>
2023-09-13 20:29:14 -0300

    feat: População de algumas tabelas do banco e criação das funções

--- src/database/popularTabelas.js
+++ src/database/popularTabelas.js
@@ -11,22 +11,36 @@ VALUES
     ('A deusa do desamor', '26.90', 'Julia ZZagonel', 'ROMANCE', 'AZT', 'Portugues'),
     ('Tweet Cute', '41.90', 'Emma Liord', 'COMEDIA', 'Wednerday the books', 'Ingles')
 `
-//Adicionando LIVROS
+//Adicionando AUTORES
 const ADD_AUTORES_DATA = `
-INSERT INTO LIVROS (NOME, PAIS)
+INSERT INTO AUTORES (NOME, PAIS, LIVROS)
 VALUES 
-    ('Carlos Duhigg', 'Brasil'),
-    ('Julia ZZagonel', 'Brasil'),
-    ('Emma Liord', 'Estados Unidos')
+    ('Carlos Duhigg', 'Brasil', ''),
+    ('Julia ZZagonel', 'Brasil', ''),
+    ('Emma Liord', 'Estados Unidos', '')
 `
-
-
+//Adicionando GENEROS
+const ADD_GENEROS_DATA = `
+INSERT INTO GENEROS (LIVROS, NOME)
+VALUES 
+    ('', 'ROMANCE'),
+    ('', 'COMEDIA'),
+    ('', 'DRAMA')
+`
+//Adicionando EDITORAS
+const ADD_EDITORAS_DATA = `
+INSERT INTO EDITORAS (NOME, EMAIL, TELEFONE)
+VALUES 
+    ('', 'objetivo.editora@gmail.com', '41 3235-6669'),
+    ('', 'aztedit@hotmail.com', '11 3007-4789'),
+    ('', 'wednerday.ed.books@yahoo.com', '1 408 323-6581')
+`
 
 
 
 /** Funções que polula via SQLite as tabelas */
 
-function populaTabelaLivros() {
+export function populaTabelaLivros() {
     Database.run(ADD_LIVROS_DATA, (error) => {
         if (error) {
             console.log("Erro ao popular tabela de Livros")
@@ -36,15 +50,70 @@ function populaTabelaLivros() {
         }
     });
 }
-export { populaTabelaLivros }
 
-function populaTabelaAutores() {
+export function populaTabelaAutores() {
     Database.run(ADD_AUTORES_DATA, (error) => {
         if (error) {
-            console.log("Erro ao popular tabela de Autores")
+            console.log("Erro ao popular tabela dos Autores")
         }
         else {
             console.log("Tabela Autores populada com sucesso")
         }
     });
 }
+
+export function populaTabelaGeneros() {
+    Database.run(ADD_GENEROS_DATA, (error) => {
+        if (error) {
+            console.log("Erro ao popular tabela de Gêneros")
+        }
+        else {
+            console.log("Tabela Gêneros populada com sucesso")
+        }
+    });
+}
+
+export function populaTabelaEditoras() {
+    Database.run(ADD_EDITORAS_DATA, (error) => {
+        if (error) {
+            console.log("Erro ao popular tabela das Editoras")
+        }
+        else {
+            console.log("Tabela Editoras populada com sucesso")
+        }
+    });
+}
+
+export function populaTabelaClientes() {
+    Database.run(ADD_CLIENTES_DATA, (error) => {
+        if (error) {
+            console.log("Erro ao popular tabela dos Clientes")
+        }
+        else {
+            console.log("Tabela Clientes populada com sucesso")
+        }
+    });
+}
+
+export function populaTabelaPagamento() {
+    Database.run(ADD_PAGAMENTO_DATA, (error) => {
+        if (error) {
+            console.log("Erro ao popular tabela de Pagamento")
+        }
+        else {
+            console.log("Tabela Pagamento populada com sucesso")
+        }
+    });
+}
+
+export function populaTabelaEstoque() {
+    Database.run(ADD_ESTOQUE_DATA, (error) => {
+        if (error) {
+            console.log("Erro ao popular tabela de Estoque")
+        }
+        else {
+            console.log("Tabela Estoque populada com sucesso")
+        }
+    });
+}
+
