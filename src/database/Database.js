05a1d6ee345f84daba4260afd0ecc74fd26cf40f
Thzzao <thiago.santos1160@gmail.com>
2023-09-13 16:43:16 -0300

    refactor: Importando as funções de criação e população do banco

--- src/database/Database.js
+++ src/database/Database.js
@@ -1,7 +1,9 @@
-// Importação do banco
+/** Script de importação do banco **/
 import sqlite3 from "sqlite3";
 import { dirname } from "path";
 import { fileURLToPath } from "url";
+import { criaTabelaLivros } from "./criarTabelas.js"
+import { populaTabelaLivros } from "./popularTabelas.js"
 
 //Torna as ações do SQLite legiveis no terminal
 sqlite3.verbose()
@@ -12,5 +14,12 @@ const filePath = dirname(fileURLToPath(import.meta.url)) + "/database.db"
 //Inicializa o banco de dados SQLite
 const Database = new sqlite3.Database(filePath)
 
-
 export default Database
+
+
+/** Roda as funções de criação de tabela e população em serie (Uma após a outra) **/
+
+Database.serialize(() => {
+    criaTabelaLivros();
+    populaTabelaLivros();
+})
