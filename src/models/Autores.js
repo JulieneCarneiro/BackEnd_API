4913ddc8baa8ddc71334c1fb9630e429cabe744f
JulieneCarneiro <juliene.scarneiro@gmail.com>
2023-09-13 11:53:17 -0300

    fix: retirado atributos emai e resumo da entidades autores

--- src/models/Autores.js
+++ src/models/Autores.js
@@ -2,8 +2,6 @@ class Autores{
     constructor(nome, pais, resumo, email, livros){
         this.nome = nome
         this.pais = pais
-        this.resumo = resumo
-        this.email = email
         this.livros = livros 
 }
 }
