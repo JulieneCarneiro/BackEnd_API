160a5255fb1cdc4ad041162159a05ab9ebc441d2
Thzzao <thiago.santos1160@gmail.com>
2023-09-13 01:15:38 -0300

    refactor: Mudança nos comentários do código

--- src/database/Database.js
+++ src/database/Database.js
@@ -3,10 +3,8 @@ import sqlite3 from "sqlite3";
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 
-
-//Torna as ações do SQLite (erros, inserções, ...) legiveis no terminal
+//Torna as ações do SQLite legiveis no terminal
 sqlite3.verbose()
-
 
 //Aponta o caminho onde será salvo o arquivo database.db
 const filePath = dirname(fileURLToPath(import.meta.url)) + "/database.db"
