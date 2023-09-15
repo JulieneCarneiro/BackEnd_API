68a1729d12b8bed1c7a45d38e90cef39fa5f60e1
laismullerrr <lais.muller@hotmail.com>
2023-09-15 12:28:49 -0300

    feat: testes para levantamento do servidor

--- src/controllers/ClientesController.js
+++ src/controllers/ClientesController.js
@@ -1,18 +1,20 @@
-class ClientesController {
-  static rotas(app) {
-    app.get("/clientes", (req, res) => {
-      res.status(200).json({ "cliente": "fulano" });
-    });
+import Clientes from "../models/Clientes.js"; // NÃO UTILIZADO AINDA
 
+class ClientesController {
+
+  static rotas(app) {
+    app.get("/", (req, res)=>{      
+      res.status(200).json("tá funcionando, confia")
+    })
+    
     app.post("/clientes", (req, res) => {
-      const body = req.body;
-      console.log(body);
-      res.status(200).json(body);
-    });
+        const body = req.body;
+        console.log(body);
+        res.status(200).json(body);
+      });
   }
 }
 
 export default ClientesController
-
 
 
