c144f2a6af56b742b06d2912ef0cc0679a26f115
laismullerrr <lais.muller@hotmail.com>
2023-09-18 12:34:09 -0300

    feat: criação das rotas delete, post e get(por id)

--- src/controllers/ClientesController.js
+++ src/controllers/ClientesController.js
@@ -1,47 +1,77 @@
-import Clientes from "../models/Clientes.js" // NÃO UTILIZADO AINDA
-import ClientesDAO from "../DAO/ClientesDAO.js"
-import ValidacaoServices from "../../services/ValidacaoServices.js"
+import Clientes from "../models/Clientes.js"; // NÃO UTILIZADO AINDA
+import ClientesDAO from "../DAO/ClientesDAO.js";
+import ValidacaoServices from "../../services/ValidacaoServices.js";
 
-class ClientesController{
+class ClientesController {
   /**
-   * Método para centralização de rotas no controller
-   * @param {Express} app 
+   *
+   * @param {Express} app
    */
-  static rotas(app){
-      /**
-       * Rota para buscar todos os usuários
-       */
-      app.get("/clientes", async (req, res)=>{
-          const clientes = await ClientesDAO.buscarTodosOsClientes()
-          res.status(200).json(clientes)
-      })
+  static rotas(app) {
+    /**
+     * BUSCA TODOS os CLIENTES
+     */
+    app.get("/clientes", async (req, res) => {
+      const clientes = await ClientesDAO.buscarTodosOsClientes();
+      res.status(200).json(clientes);
+    });
 
-      ////////////////  CÓDIGO TESTE 
-      // app.post("/clientes", (req, res)=>{
-      //           const body = Object.values(req.body)
-      //           const clienteModelado = new Clientes(...body)
-      //           ClientesDAO.inserirCliente(clienteModelado)
-      //           res.status(200).json({
-      //               error: false,
-      //               message: "DEU BOA FAMILIA"
-      //           })
-      //       })
-      
-      /**
-       * Rota para buscar usuários pelo id
-       */
-      
-    }}
+    /**
+     * BUSCA pelo ID                        ///////////SEM VALIDAÇÃO TA FUNCIONANDOOOOOOOOOOOOO
+     */
+    app.get("/clientes/:id", async (req, res) => {
+      const id = req.params.id;
+      const resposta = await ClientesDAO.buscarClientePorId(id);
+      if (resposta) {
+        res.status(200).json(resposta);
+      } else {
+        res
+          .status(404)
+          .json({
+            error: true,
+            message: `Cliente com o id ${id} não encontrado`,
+          });
+      }
+    });
 
-export default ClientesController
+    /**
+     * DELETA por ID                      ///////////SEM VALIDAÇÃO TA FUNCIONANDO --- MAS DA PRA MELHORAR A RESPOSTA
+     */
+    app.delete("/clientes/:id", async (req, res) => {
+      const id = req.params.id;
+      ClientesDAO.deletarClientePorId(id);
+      res.status(200).json({ error: false });
+    });
+
+    /**
+     * INSERE                           ///////////SEM VALIDAÇÃO TA FUNCIONANDOOOOOOOOOOOOO
+     */
+    app.post("/clientes", async (req, res) => {
+      const body = Object.values(req.body);
+      const clienteModelado = new Clientes(...body);
+      try {
+        await ClientesDAO.inserirCliente(clienteModelado);
+        res.status(201).json({
+          error: false,
+          message: "Cliente inserido com sucesso!",
+        });
+      } catch (error) {
+        res
+          .status(503)
+          .json({ error: true, message: `Servidor indisponível no momento` });
+      }
+    });
+  }}
+
+export default ClientesController;
 // class ClientesController {
 
 //   static rotas(app) {
 
-//     app.get("/clientes", (req, res)=>{      
+//     app.get("/clientes", (req, res)=>{
 //       res.status(200).json(Clientes)
 //     })
-    
+
 //       app.post("/clientes", (req, res)=>{
 //         const body = Object.values(req.body)
 //         const clienteModelado = new Clientes(...body)
@@ -72,8 +102,4 @@ export default ClientesController
 //     //     }
 //     // })
 //   }
-// }
-
-// export default ClientesController
-
 
