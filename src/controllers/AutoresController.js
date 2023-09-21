edbcdc11e6024e13836515c655feb27f06377220
laismullerrr <lais.muller@hotmail.com>
2023-09-21 19:22:29 -0300

    feat: adição das validações nas rotas de Autores

--- src/controllers/AutoresController.js
+++ src/controllers/AutoresController.js
@@ -1,5 +1,6 @@
 import Autores from "../models/Autores.js"; 
 import AutoresDAO from "../DAO/AutoresDAO.js";
+import ValidacaoServicesAutor from "../services/AutoresServices.js";
 
 class AutoresController {
   /**
@@ -7,71 +8,95 @@ class AutoresController {
    * @param {Express} app
    */
   static rotas(app) {
+    
     /**
      * BUSCA TUDO
      */
     app.get("/autores", async (req, res) => {
       try {
+        // verifica se existem autores no banco de dados
         const autores = await AutoresDAO.buscarTodosOsAutores();
         if (autores) {
           res.status(200).json(autores);
-        } else {
+        } else { 
+          // Se não existirem autores, retorna um erro 404
           res.status(404).json({
             error: true,
             message: "Nenhum autor encontrado",
           });
         }
       } catch (error) {
+        // se não encontrar o autor (caso inesperado), retorna um erro 500
         res.status(500).json({
           error: true,
           message: "Ocorreu um erro ao buscar os autores",
         });
       }
     });
+    
 
     /**
      * BUSCA pelo ID                        
      */
     app.get("/autores/:id", async (req, res) => {
       const id = req.params.id;
-      const resposta = await AutoresDAO.buscarAutorPorId(id);
-      if (resposta) {
-        res.status(200).json(resposta);
+      // Verifica se o autor com o ID existe
+      const exists = await ValidacaoServicesAutor.validarExistenciaAutor(id);
+      if (exists) {
+        // se o autor existe, executa a busca
+        const resposta = await AutoresDAO.buscarAutorPorId(id);
+        if (resposta) {
+          // se encontrar o autor, retorna os dados dele
+          res.status(200).json(resposta);
+        } else {
+          // se não encontrar o autor (caso inesperado), retorna um erro 500
+          res.status(500).json({
+            error: true,
+            message: `Ocorreu um erro ao buscar o autor com o ID ${id}`,
+          });
+        }
       } else {
-        res.status(404).json({
-          error: true,
-          message: `Autor com o id ${id} não encontrado`,
-        });
+        // se o autor não existe, retorna um erro 404
+        res.status(404).json({ error: true, message: `Autor não encontrado para o ID ${id}` });
       }
     });
-    /**    
-    //* DELETA por ID                      ///////////DA PRA MELHORAR A RESPOSTA
-    */
 
+
+    /**    
+    //* DELETA por ID       
+    */
     app.delete("/autores/:id", async (req, res) => {
       const id = req.params.id;
-      AutoresDAO.deletarAutorPorId(id);
-      res
-        .status(200)
-        .json({ error: false, message: `Autor deletado com sucesso!` });
+      const exists = await ValidacaoServicesAutor.validarExistenciaAutor(id);
+      if (exists) {
+        // se o autor existe, confirma
+        AutoresDAO.deletarAutorPorId(id);
+        res.status(200).json({ error: false, message: `Autor deletado com sucesso!` });
+      } else {
+        // Se o autor não existe, retorna erro 404
+        res.status(404).json({ error: true, message: `Autor não encontrado para o ID ${id}` });
+      }
     });
+
 
     /**
      * INSERE                          
      */
     app.post("/autores", async (req, res) => {
-      const body = Object.values(req.body);
-      const autorModelado = new Autores(...body);
+      const body = req.body;
+      if (!ValidacaoServicesAutor.validaAutor(body.NOME, body.PAIS, body.LIVROS)) {
+        return res.status(400).json({ error: true, message: "Campos inválidos na solicitação" });
+      }
       try {
-        await AutoresDAO.inserirAutor(autorModelado);
+        await AutoresDAO.inserirAutor(body);
         res.status(201).json({
           error: false,
           message: "Autor inserido com sucesso!",
         });
       } catch (error) {
-        res
-          .status(503)
-          .json({ error: true, message: `Servidor indisponível no momento` });
+        // tratamento do erro 409, quando um ID igual já existe 
+        console.error("Erro no servidor:", error);
+        res.status(409).json({ error: true, message: "Já existe um ID com o mesmo valor único" });
       }
     });
   }
