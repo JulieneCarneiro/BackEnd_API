3e12e009e200702aa3c44cb4b9b2aead3fddb49e
laismullerrr <lais.muller@hotmail.com>
2023-09-21 15:17:05 -0300

    feat: novas rotas get, delete, e post em Generos

--- src/controllers/GenerosController.js
+++ src/controllers/GenerosController.js
@@ -1,57 +1,79 @@
-import express from 'express';
+import Generos from '../models/Generos.js'
 import GenerosDAO from '../DAO/GenerosDAO.js';
 
-const router = express.Router();
-const generosDAO = new GenerosDAO();
-
-router.get('/', async (req, res) => {
-  try {
-    const generos = await generosDAO.buscarTodosOsGeneros();
-    res.status(200).json(generos);
-  } catch (error) {
-    res.status(500).json({ error: true, message: 'Erro ao buscar generos' });
-  }
-});
-
-router.get('/:id', async (req, res) => {
-  const id = req.params.id;
-  try {
-    const genero = await generosDAO.buscarGeneroPorId(id);
-    if (genero) {
-      res.status(200).json(genero);
-    } else {
-      res.status(404).json({
-        error: true,
-        message: `Genero com o id ${id} não encontrado`,
-      });
-    }
-  } catch (error) {
-    res.status(500).json({ error: true, message: 'Erro ao buscar genero por ID' });
-  }
-});
-
-router.delete('/:id', async (req, res) => {
-  const id = req.params.id;
-  try {
-    await generosDAO.deletarGeneroPorId(id);
-    res.status(200).json({ error: false });
-  } catch (error) {
-    res.status(500).json({ error: true, message: 'Erro ao deletar genero por ID' });
-  }
-});
-
-router.post('/', async (req, res) => {
-  const body = req.body;
-  try {
-    const generoInserido = await generosDAO.inserirGenero(body);
-    res.status(201).json({
-      error: false,
-      message: 'Genero inserido com sucesso!',
-      genero: generoInserido,
+class GenerosController {
+  /**
+   * Método para centralização de rotas no controller
+   * @param {Express} app
+   */
+  static rotas(app) {
+    
+    /**
+     * BUSCA TUDO
+     */
+    app.get("/generos", async (req, res) => {
+      try {
+      const generos = await GenerosDAO.buscarTodosOsGeneros();
+      res.status(200).json(generos);} catch (error) {
+        if (error instanceof Error) {
+          res.status(500).json({
+            error: true,
+            message: "Nenhum gênero encontrado", 
+            details: error.message,
+          });
+        } else {
+          
+          res.status(500).json({
+            error: true,
+            message: "Ocorreu um erro ao buscar os gêneros.",
+          });
+        }
+      }
     });
-  } catch (error) {
-    res.status(500).json({ error: true, message: 'Erro ao inserir genero' });
-  }
-});
+    /**
+     * BUSCA pelo ID                      
+     */
+    app.get("/generos/:id", async (req, res) => {
+      const id = req.params.id;
+      const resposta = await GenerosDAO.buscarGeneroPorId(id);
+      if (resposta) {
+        res.status(200).json(resposta);
+      } else {
+        res.status(404).json({
+          error: true,
+          message: `Genero com o id ${id} não encontrada`,
+        });
+      }
+    });
 
-export default router;
+    /**    
+    //* DELETA por ID                     
+    */
+    app.delete("/generos/:id", async (req, res) => {
+      const id = req.params.id;
+      GenerosDAO.deletarGeneroPorId(id);
+      res.status(200).json({ error: false });
+    });
+
+    /**
+     * INSERE                      
+     */
+    app.post("/generos", async (req, res) => {
+      const body = Object.values(req.body);
+      const novoGenero = new Generos(...body);
+      try {
+        await GenerosDAO.inserirGenero(novoGenero);
+        res.status(201).json({
+          error: false,
+          message: "Genero inserido com sucesso!",
+        });
+      } catch (error) {
+        res
+          .status(503)
+          .json({ error: true, message: `Servidor indisponível no momento` });
+      }
+    });
+  }
+}
+
+export default GenerosController;
