0b6436b216641a5bdd61d32bd82aace3d4236a71
laismullerrr <lais.muller@hotmail.com>
2023-09-21 20:37:09 -0300

    feat: importação das rotas no app.js

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 import express from "express";
 import cors from "cors";
 import ClientesController from "./src/controllers/ClientesController.js"
+import AutoresController from "./src/controllers/AutoresController.js"
+import LivrosController from "./src/controllers/LivrosController.js";
+import PedidosController from "./src/controllers/PedidosController.js";
+import GenerosController from "./src/controllers/GenerosController.js";
+import EditorasController from './src/controllers/EditorasController.js'
 
 const app = express()
 const port = process.env.PORT || 3000
@@ -12,8 +17,15 @@ app.listen(port, () => {
 
 app.use(express.json())
 
-
+AutoresController.rotas(app)
 ClientesController.rotas(app)
+LivrosController.rotas(app)
+PedidosController.rotas(app)
+GenerosController.rotas(app)
+EditorasController.rotas(app)
+
+
+
 
 
 app.use(cors('*'))
