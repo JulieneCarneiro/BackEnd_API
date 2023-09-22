641ffba5acaa0848020d2c4274fdc1b4483d91f7
laismullerrr <lais.muller@hotmail.com>
2023-09-22 19:48:44 -0300

    feat: validação nas rotas delete, put e post

--- src/controllers/PedidosController.js
+++ src/controllers/PedidosController.js
@@ -1,7 +1,6 @@
-import Pedidos from "../models/Pedidos.js"
-import PedidosDAO from '../DAO/PedidosDAO.js';
-import ValidacaoServicesPedidos from '../services/PedidosServices.js'
-
+import Pedidos from "../models/Pedidos.js";
+import PedidosDAO from "../DAO/PedidosDAO.js";
+import ValidacaoServicesPedidos from "../services/PedidosServices.js";
 
 class PedidosController {
   /**
@@ -9,21 +8,20 @@ class PedidosController {
    * @param {Express} app
    */
 
-
   static rotas(app) {
-
     /**
      * BUSCA TODOS os PEDIDOS
      */
     app.get("/pedidos", async (req, res) => {
       try {
-      const pedidos = await PedidosDAO.buscarTodosOsPedidos();
-      res.status(200).json(pedidos);} catch (error) {
+        const pedidos = await PedidosDAO.buscarTodosOsPedidos();
+        res.status(200).json(pedidos);
+      } catch (error) {
         if (error instanceof Error) {
           res.status(500).json({
             error: true,
-            message: "Nenhum pedido encontrado", 
-            details: error.message,            
+            message: "Nenhum pedido encontrado",
+            details: error.message,
           });
         } else {
           res.status(500).json({
@@ -34,14 +32,15 @@ class PedidosController {
       }
     });
 
-
     /**
-     * BUSCA pelo ID                FEITO    
+     * BUSCA pelo ID                FEITO
      */
     app.get("/pedidos/:id", async (req, res) => {
       const id = req.params.id;
       // Verifica se o pedido com o ID existe
-      const isValid = await ValidacaoServicesPedidos.validarExistenciaPedido(id);
+      const isValid = await ValidacaoServicesPedidos.validarExistenciaPedido(
+        id
+      );
       if (isValid) {
         // se o pedido existe, executa a busca
         const resposta = await PedidosDAO.buscarPedidoPorId(id);
@@ -50,7 +49,12 @@ class PedidosController {
           res.status(200).json(resposta);
         } else {
           // se o pedido não existe, retorna um erro 404
-        res.status(404).json({ error: true, message: `Pedido não encontrado para o ID ${id}` });
+          res
+            .status(404)
+            .json({
+              error: true,
+              message: `Pedido não encontrado para o ID ${id}`,
+            });
         }
       } else {
         // se não encontrar o pedido (caso inesperado), retorna um erro 500
@@ -60,89 +64,115 @@ class PedidosController {
         });
       }
     });
-    
 
-/**
-     * BUSCA pelo pagto               
+    /**
+     * BUSCA pelo pagto
      */
-app.get("/pedidos/pagamento/:pagamento", async (req, res) => {
-  const pagamento = req.params.pagamento;
-  const resposta = await PedidosDAO.buscarPedidosPorPagamento(pagamento);
-  if (resposta) {
-    res.status(200).json(resposta);
-  } else {
-    res
-      .status(404)
-      .json({
-        error: true,
-        message: `Não encontramos pedidos com o pagamento ${pagamento}`,
-      });
+    app.get("/pedidos/pagamento/:pagamento", async (req, res) => {
+      const pagamento = req.params.pagamento;
+      const resposta = await PedidosDAO.buscarPedidosPorPagamento(pagamento);
+      if (resposta) {
+        res.status(200).json(resposta);
+      } else {
+        res.status(404).json({
+          error: true,
+          message: `Não encontramos pedidos com o pagamento ${pagamento}`,
+        });
+      }
+    });
+
+    /**
+     * DELETA por ID                      ///////////SEM VALIDAÇÃO TA FUNCIONANDO --- MAS DA PRA MELHORAR A RESPOSTA
+     */
+    app.delete("/pedidos/:id", async (req, res) => {
+      const id = req.params.id;
+      PedidosDAO.deletarPedidoPorId(id);
+      res
+        .status(200)
+        .json({ error: false, message: `Livro excluído com sucesso!` });
+    });
+
+    /**
+     * INSERE
+     */
+    app.post("/pedidos", async (req, res) => {
+      const body = req.body;
+
+      if (
+        !ValidacaoServicesPedidos.validaCamposPedido(
+          body.TITULO,
+          body.PAGAMENTO
+        )
+      ) {
+        return res
+          .status(400)
+          .json({ error: true, message: `Campos inválidos` });
+      }
+      try {
+        const exists = await ValidacaoServicesPedidos.validarExistenciaPedido(
+          body.ID
+        );
+        if (exists) {
+          const pedidoModelado = new Pedidos(
+            body.ID,
+            body.CLIENTE,
+            body.TITULO,
+            body.QUANTIDADE,
+            body.VALOR,
+            body.PAGAMENTO
+          );
+          await PedidosDAO.inserirPedido(pedidoModelado);
+          res.status(201).json({
+            message: "Pedido criado com sucesso!",
+          });
+        } else {
+          res
+            .status(400)
+            .json({ error: true, message: `Pedido não encontrado` });
+        }
+      } catch (error) {
+        console.error("Erro no servidor:", error);
+        res
+          .status(503)
+          .json({ error: true, message: `Servidor indisponível no momento` });
+      }
+    });
+
+    // /**
+    //  * ATUALIZA por ID
+    //  */
+    app.put("/pedidos/:id", async (req, res) => {
+      const id = req.params.id;
+      const body = req.body;
+      const exists = await ValidacaoServicesPedidos.validarExistenciaPedido(id);
+      const isValid = ValidacaoServicesPedidos.validaCamposPedido(
+        body.TITULO,
+        body.PAGAMENTO
+      );
+      if (exists) {
+        if (isValid) {
+          const pedidoAtualizado = {
+            CLIENTE: body.CLIENTE,
+            TITULO: body.TITULO,
+            QUANTIDADE: body.QUANTIDADE,
+            VALOR: body.VALOR,
+            PAGAMENTO: body.PAGAMENTO,
+          };
+          await PedidosDAO.atualizarPedidoPorId(id, pedidoAtualizado);
+          res
+            .status(204)
+            .json({ error: false, message: `Pedido atualizado com sucesso!` });
+        } else {
+          res.status(400).json({ error: true, message: `Campos inválidos` });
+        }
+      } else {
+        res.status(404).json({
+          error: true,
+          message: `Pedido não encontrado para o ID ${id}`,
+        });
+      }
+    });
   }
-});
-
-/**
- * DELETA por ID                      ///////////SEM VALIDAÇÃO TA FUNCIONANDO --- MAS DA PRA MELHORAR A RESPOSTA
- */
-app.delete("/pedidos/:id", async (req, res) => {
-  const id = req.params.id;
-  PedidosDAO.deletarPedidoPorId(id);
-  res.status(200).json({ error: false, message: `Livro excluído com sucesso!` });
-});
-
-/**
- * INSERE              
- */
-  app.post("/pedidos", async (req, res) => {
-  const body = req.body;
-
-  if (!ValidacaoServicesPedidos.validaCamposPedido(body.TITULO, body.PAGAMENTO)) {
-    return res.status(400).json({ error: true, message: `Campos inválidos` });
-  }
-  try {
-    const exists = await ValidacaoServicesPedidos.validarExistenciaPedido(body.ID);
-    if (exists) {
-      const pedidoModelado = new Pedidos(body.ID, body.CLIENTE, body.TITULO, body.QUANTIDADE, body.VALOR, body.PAGAMENTO);
-      await PedidosDAO.inserirPedido(pedidoModelado);
-      res.status(201).json({
-        error: false,
-        message: "Pedido criado com sucesso!"
-      });
-    } else {
-      res.status(400).json({ error: true, message: `Pedido não encontrado` });
-    }
-  } catch (error) {
-    console.error("Erro no servidor:", error);
-    res.status(503).json({ error: true, message: `Servidor indisponível no momento` });
-  }
-});
-
-
-
-
-// /**
-//  * ATUALIZA por ID             
-//  */
-app.put("/pedidos/:id", async (req, res) => {
-  const id = req.params.id;
-  const body = req.body;
-
-  try {
-    const exists = await ValidacaoServicesPedidos.validarExistenciaPedido(id);
-
-    if (exists) {
-      const unidadeModelada = new Pedidos(body.CLIENTE, body.TITULO, body.QUANTIDADE, body.VALOR, body.PAGAMENTO);
-      PedidosDAO.atualizarPedidoPorId(id, unidadeModelada);
-      res.status(204).json({ error: false, message: `Cliente inserido com sucesso` });
-    } else {
-      console.error("Erro no servidor:", error);
-      res.status(400).json({ error: true, message: `Campos inválidos` });
-    }
-  } catch (error) {
-    console.error("Erro no servidor:", error);
-    res.status(503).json({ error: true, message: `Servidor indisponível no momento` });
-  }
-});
-
-}}
+}
 
 export default PedidosController;
