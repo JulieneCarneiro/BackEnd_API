4461ea6988a6593a835be7483a0ad1794a231d70
Thzzao <thiago.santos1160@gmail.com>
2023-09-22 01:22:38 -0300

    feat: Criação das validações de gêneros

--- src/Services/GenerosServices.js
+++ src/Services/GenerosServices.js
@@ -1,4 +1,47 @@
-class ValidacoesGeneros {
+import GenerosDAO from "../DAO/GenerosDAO.js"
+
+class ValidacaoServicesGeneros {
+    /**
+ * Método que valida a existencia do gênero na base de dados
+ * @param {string} id 
+ * @returns {boolean}
+ */
+    static validarExistenciaGenero(id) {
+        const genero = GenerosDAO.buscarGeneroPorId(id)
+        if (genero) {
+            return true
+        } else {
+            return false
+        }
+    }
+    /**
+     * Método de validação do nome do gênero
+     * @param {string} nome 
+     * @returns {boolean}
+     */
+    static validaNome(nome) {
+        return typeof nome == "string" && nome.length > 3
+    }
+}
+export default ValidacaoServicesGeneros;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/**
     static validaId(id) {
         // Verifica se o ID é uma string válida
         if (!id || typeof id !== 'string') {
@@ -20,3 +63,4 @@ class ValidacoesGeneros {
         }
     }
 }
+**/
