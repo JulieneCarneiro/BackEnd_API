a2c7726624a7ecd6635a794c3a8418fc741eaaec
laismullerrr <lais.muller@hotmail.com>
2023-09-22 18:53:09 -0300

    feat: criação de validações para Editoras

--- src/Services/EditorasSevices.js
+++ src/Services/EditorasSevices.js
@@ -1,97 +1,95 @@
+
 import Editoras from "../models/Editoras.js";
 import EditorasDAO from "../DAO/EditorasDAO.js";
 
-// Classe ValidacaoEditoras definida antes do EditorasController
-class ValidacaoEditoras {
-    // Método para validar a inserção de dados da editora
-    static validaInsercaoEditora(editoraModelada) {
-        // Verifica se editoraModelada é um objeto válido
-        if (!editoraModelada || typeof editoraModelada !== "object") {
-            throw new Error("Dados da editora inválidos.");
-        }
-
-        // Verifica se o nome da editora é uma string válida e obrigatória
-        if (!editoraModelada.nome || typeof editoraModelada.nome !== "string") {
-            throw new Error("Nome da editora é obrigatório e deve ser uma string.");
-        }
-
-        // Verifica se o país da editora é uma string válida e obrigatória
-        if (
-            !editoraModelada.pais ||
-            typeof editoraModelada.pais !== "string"
-        ) {
-            throw new Error(
-                "País da editora é obrigatório e deve ser uma string."
-            );
-        }
-
-        // Se todas as validações passarem, os dados são considerados válidos
-        return true;
-    }
-}
-
-class EditorasController {
+class ValidacaoServicesEditora {
     /**
-     * Método para centralização de rotas no controller
-     * @param {Express} app
+     * Método que valida a existencia do gênero na base de dados
+     * @param {string} id 
+     * @returns {boolean}
      */
-    static rotas(app) {
-        /**
-         * BUSCA TUDO
-         */
-        app.get("/editoras", async (req, res) => {
-            const editoras = await EditorasDAO.buscarTodasAsEditoras();
-            res.status(200).json(editoras);
-        });
+    static validarExistenciaEditora(id) {
+        const genero = EditorasDAO.buscarEditoraPorId(id)
+        if (genero) {
+            return true
+        } else {
+            return false
+        }
+    }
 
-        /**
-         * BUSCA pelo ID
-         */
-        app.get("/editoras/:id", async (req, res) => {
-            const id = req.params.id;
-            const resposta = await EditorasDAO.buscarEditoraPorId(id);
-            if (resposta) {
-                res.status(200).json(resposta);
-            } else {
-                res.status(404).json({
-                    error: true,
-                    message: `Editora com o id ${id} não encontrada`,
-                });
-            }
-        });
 
-        /**    
-        //* DELETA por ID
-        */
-        app.delete("/editoras/:id", async (req, res) => {
-            const id = req.params.id;
-            EditorasDAO.deletarEditoraPorId(id);
-            res.status(200).json({ error: false, message: `Editora deletada com sucesso!` });
-        });
+    /**
+     * Método de validação de nome
+     * @param {string} nome 
+     * @returns {boolean}
+     */
+    static validaNome(nome){
+        return typeof nome == "string" && nome.length > 2
+    }
 
-        /**
-         * INSERE
-         */
-        app.post("/editoras", async (req, res) => {
-            const body = Object.values(req.body);
-            const editoraModelada = new Editoras(...body);
-            try {
-                // Antes de inserir, valide os dados usando a classe ValidacaoEditoras
-                ValidacaoEditoras.validaInsercaoEditora(editoraModelada);
+    /**
+     * Método para validação de email
+     * @param {string} email 
+     * @returns {boolean}
+     */
+    static validaEmail(email){
+        return typeof email == "string" && email.length > 2
+    }
 
-                await EditorasDAO.inserirEditora(editoraModelada);
-                res.status(201).json({
-                    error: false,
-                    message: "Editora inserida com sucesso!",
-                });
-            } catch (error) {
-                res.status(503).json({
-                    error: true,
-                    message: `Servidor indisponível no momento`,
-                });
-            }
-        });
+    /**
+     * Método para validação de telefone
+     * @param {string} telefone 
+     * @returns {boolean}
+     */
+    static validaTelefone(telefone) {
+        // Verifica se o telefone é uma string e tem um comprimento mínimo de 10 caracteres (assumindo que inclui o DDD)
+        return typeof telefone === "string" && telefone.length >= 10;
+    }
+
+    /**
+     * Método para validação de todos os campos
+     * @param {string} nome 
+     * @param {string} email 
+     * @param {string} telefone 
+     * @returns 
+     */
+    static validaCamposEditora(nome, email, telefone){
+        const isValid = this.validaNome(nome) && this.validaEmail(email) && this.validaTelefone(telefone)
+        return isValid
     }
 }
+export default ValidacaoServicesEditora;
 
-export default EditorasController;
+
+
+
+// // Classe ValidacaoEditoras definida antes do EditorasController
+// class ValidacaoEditoras {
+//     // Método para validar a inserção de dados da editora
+//     static validaInsercaoEditora(editoraModelada) {
+//         // Verifica se editoraModelada é um objeto válido
+//         if (!editoraModelada || typeof editoraModelada !== "object") {
+//             throw new Error("Dados da editora inválidos.");
+//         }
+
+//         // Verifica se o nome da editora é uma string válida e obrigatória
+//         if (!editoraModelada.nome || typeof editoraModelada.nome !== "string") {
+//             throw new Error("Nome da editora é obrigatório e deve ser uma string.");
+//         }
+
+//         // Verifica se o país da editora é uma string válida e obrigatória
+//         if (
+//             !editoraModelada.pais ||
+//             typeof editoraModelada.pais !== "string"
+//         ) {
+//             throw new Error(
+//                 "País da editora é obrigatório e deve ser uma string."
+//             );
+//         }
+
+//         // Se todas as validações passarem, os dados são considerados válidos
+//         return true;
+//     }
+// }
+
+// export default ValidacaoEditorass
