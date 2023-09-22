c5544f9b31ce5608b57f51dc1c9280ee91f03f6e
laismullerrr <lais.muller@hotmail.com>
2023-09-22 18:21:37 -0300

    fix: ajustes nas funções de deletar/atualizar por id

--- src/DAO/DAO.js
+++ src/DAO/DAO.js
@@ -62,37 +62,35 @@ class DAO{
      * @param {id} id 
      * @returns {Promise<void>}
      */
-    static deletarPorId(query, id) {
-      return new Promise((resolve, reject) => {
-          Database.run(query, id, function (error) {
-              if (error) {
-                  console.error(error);
-                  reject(error);
-              } else {
-                  resolve();
-              }
-          });
-      });
+    static deletarPorId(query, id){
+        return new Promise((resolve, reject)=>{
+            Database.all(query, id, (error, rows)=>{
+                if(error){
+                    console.log(error)
+                } else {
+                    resolve(rows)
+                }
+            })
+        })
     }
-
     /**
      * UPDATE por ID
      * @param {string} query 
      * @param {string} id 
      * @returns {Promise<void>}
      */
-    static atualizarPorId(query, id) {
-      return new Promise((resolve, reject) => {
-          Database.run(query, id, function (error) {
-              if (error) {
-                  console.error(error);
-                  reject(error);
-              } else {
-                  resolve();
-              }
-          });
-      });
-    }
+    static atualizarPorId(query, data, id) {
+    const values = Object.values(data);
+    return new Promise((resolve, reject) => {
+        Database.run(query, [...values, id], (error, rows) => {
+            if (error) {
+                reject(error);
+            } else {
+                resolve(rows);
+            }
+        });
+    });
+}
 
 }
 
