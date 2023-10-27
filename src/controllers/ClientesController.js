a063bc95ed86893a0a8d776f90d4777fe240f0dd
laismullerrr <lais.muller@hotmail.com>
2023-10-27 20:14:31 -0300

    feat: adição da rota patch

--- src/controllers/ClientesController.js
+++ src/controllers/ClientesController.js
@@ -37,35 +37,54 @@ class ClientesController {
             }
         });
 
+        // app.put("/clientes/:id", async (req, res) => {
+        //     const id = req.params.id
+        //     const body = Object.entries(req.body)
+        //     try {
+        //         if (!body || !Array.isArray(body)) {
+        //         throw new Error("Corpo da solicitação inválido");
+        //         }
+        //         const cliente = req.body
+        //         // if (!cliente._id) {
+        //         //     throw new Error("Cliente não encontrado para esse id")
+        //         // }
+        //         body.forEach((elemento) => cliente[elemento[0]] = elemento[1])
+        //         delete cliente._id
+        //         ClientesServices.validaCamposCliente(cliente.nome, cliente.pais, cliente.livros)
+        //         const resposta = await ClientesRepository.atualizarClientePorId(id, cliente)
+        //         res.status(200).json(resposta)
+        //     } catch (error) {
+        //         res.status(400).json({ message: error.message, id })
+        //     }
+        // })
 
-        app.put("/clientes/:id", async (req, res) => {
-            const id = req.params.id
-            const body = Object.entries(req.body)
-            try {
-                const cliente = req.body
-                // if (!cliente._id) {
-                //     throw new Error("Cliente não encontrado para esse id")
-                // }
-                body.forEach((elemento) => cliente[elemento[0]] = elemento[1])
-                delete cliente._id
-                ClientesServices.validaCamposCliente(cliente.nome, cliente.pais, cliente.livros)
-                const resposta = await ClientesRepository.atualizaClientePorId(id, cliente)
-                res.status(200).json(resposta)
-            } catch (error) {
-                res.status(400).json({ message: error.message, id })
-            }
-        })
-
+        
         app.patch("/clientes/:id", async (req, res) => {
-            const id = req.params.id
-            const entries = Object.entries(req.body)
             try {
-                const cliente = req.body
-                await ClientesServices.validaCamposCliente(entries)
-                const resposta = await ClientesRepository.atualizaClientePorId(id, cliente)
-                res.status(200).json(resposta)
+                const cliente = await ClientesRepository.buscarClientePorId(req.params.id)
+                if (!cliente) {
+                    throw new Error("ID do cliente não encontrado")
+                }
+                const data = req.body
+                if (data._id || data.CPF || data.__v){
+                    throw new Error("Contém um atributo que não pode ser alterado")
+                }
+                if (data.nome){
+                    ClientesServices.validaNome(data.nome)
+                }
+                if (data.endereco){
+                    ClientesServices.validaEndereco(data.endereco)
+                }
+                if (data.telefone){
+                    ClientesServices.validaTelefone(data.telefone)
+                }
+                if (data.email){
+                    ClientesServices.validaEmail(data.email)
+                }
+                await ClientesRepository.atualizarClientePorId(req.params.id, data)
+                res.status(200).json({ message: "Cliente atualizado com sucesso" })
             } catch (erro) {
-                res.status(400).json({ message: erro.message, id })
+                res.status(400).json({ message: erro.message, id: req.params.id })
             }
         })
 
