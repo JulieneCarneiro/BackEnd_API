c7fd64786e06df06f6ff6386823da3bb6e599183
laismullerrr <lais.muller@hotmail.com>
2023-10-27 16:43:31 -0300

    fix: ajustes nas rotas post, get e delete

--- src/Services/ClientesServices.js
+++ src/Services/ClientesServices.js
@@ -2,53 +2,103 @@ import ClientesRepository from "../Repository/ClientesRepository.js"
 
 class ClientesServices {
 
-    static async validaNome(nome) {
-        const cliente = await ClientesRepository.buscarClientePorNome(nome);
-        if (!cliente && nome.length > 2)  {
-          return true;
-        } else {
-          throw new Error("Cliente já cadastrado.");
-        }
-      }
-    
-      static async validaEmail(email) {
-        const regex = /^[a-z0-9.]+@[a-z0-9]+\.[a-z]+(\.[a-z]+)?$/i
-        if (regex.test(email)) {
-            const VerificaCliente = await ClientesRepository.buscarClientePorEmail(email)
-            if (VerificaCliente) {
-                throw new Error("Email já cadastrado.")
-            }
-            return true
-
-        }
-
-        throw new Error("Email inválido, favor rever a requisição.")
-
-    }
-
-    static validaTelefone(telefone) {
-        const tel = parseInt(telefone)
-        if (tel != telefone || telefone.length < 10 || telefone.length > 12) {
-            throw new Error("Telefone inválido, favor rever a requisição.")
-        }
-
-        return true
-    }
   
-  static validaEndereco(endereco) {
-    return endereco.length >= 5;
+static validaNome(nome){
+      const regexNome = /^[a-zA-ZÀ-ú\s']+$/;
+      if (regexNome.test(nome)){
+          return true
+      } else {
+          throw new Error("Nome inválido, apenas letras")
+      }
   }
-
-  static async validaCamposCliente(nome, email, telefone, endereco) {
-    try {
-       await ClientesServices.validaNome(nome) 
-       ClientesServices.validaEmail(email) 
-       ClientesServices.validaTelefone(telefone) 
-       ClientesServices.validaEndereco(endereco);
-    } catch (error) {
-        throw error
-    } 
+static validaEndereco(endereco){
+  if (endereco.length >= 3){
+      return true
+  } else {
+      throw new Error("Endereço inválido, deve ter no mínimo 3 caracteres")
   }
 }
 
+static validaTelefone(telefone){
+  const numeroLimpo = telefone.replace(/[^\d]/g, '');
+  const telefoneInt = parseInt(numeroLimpo)
+  const regexTelefone = /^[0-9]{10,11}$/
+
+  if (regexTelefone.test(telefoneInt)){
+      return true
+  } else {
+      throw new Error("Telefone inválido")
+  }
+}
+
+static validaEmail(email) {
+  const regexEmail = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+  if (regexEmail.test(email)) {
+      return true;
+  } else {
+      throw new Error("E-mail inválido");
+  }
+}
+
+static validaCamposCliente( nome, endereco, telefone, email){
+  const isValid = this.validaNome(nome) && this.validaEndereco(endereco) && this.validaTelefone(telefone) && this.validaEmail(email)
+  if (isValid) {
+      return true;
+  } else {
+      throw new Error("Dados inválido para o cliente");
+  }
+}
+}
+
 export default ClientesServices;
+
+//     static async validaNome(nome) {
+//         const cliente = await ClientesRepository.buscarClientePorNome(nome);
+//         if (!cliente && nome.length > 2)  {
+//           return true;
+//         } else {
+//           throw new Error("Cliente já cadastrado.");
+//         }
+//       }
+
+  
+    
+//       static async validaEmail(email) {
+//         const regex = /^[a-z0-9.]+@[a-z0-9]+\.[a-z]+(\.[a-z]+)?$/i
+//         if (regex.test(email)) {
+//             const VerificaCliente = await ClientesRepository.buscarClientePorEmail(email)
+//             if (VerificaCliente) {
+//                 throw new Error("Email já cadastrado.")
+//             }
+//             return true
+
+//         }
+
+//         throw new Error("Email inválido, favor rever a requisição.")
+
+//     }
+
+//     static validaTelefone(telefone) {
+//         const tel = parseInt(telefone)
+//         if (tel != telefone || telefone.length < 10 || telefone.length > 12) {
+//             throw new Error("Telefone inválido, favor rever a requisição.")
+//         }
+
+//         return true
+//     }
+  
+//   static validaEndereco(endereco) {
+//     return endereco.length >= 5;
+//   }
+
+//   static async validaCamposCliente(nome, email, telefone, endereco) {
+//     try {
+//        await ClientesServices.validaNome(nome) 
+//        ClientesServices.validaEmail(email) 
+//        ClientesServices.validaTelefone(telefone) 
+//        ClientesServices.validaEndereco(endereco);
+//     } catch (error) {
+//         throw error
+//     } 
+//   }
+// }
--- src/controllers/ClientesController.js
+++ src/controllers/ClientesController.js
@@ -1,50 +1,44 @@
 import ClientesRepository from "../Repository/ClientesRepository.js";
 import ClientesServices from "../Services/ClientesServices.js";
+import Clientes from "../models/Clientes.js";
 
 class ClientesController {
     static rotas(app) {
 
-        app.post("/clientes", async (req, res) => {
-            try {
-                await ClientesServices.validaCamposCliente(req.body.nome, req.body.email, req.body.telefone, req.body.endereco)
-
-                const cliente = req.body
-
-                const inserir = await ClientesRepository.inserirCliente(cliente)
-
-                res.status(201).json(inserir)
-            } catch (error) {
-                if (error.message == "Cliente já cadastrado.") {
-                    res.status(406).json({ message: error.message })
-                }
-
-                res.status(400).json({ message: error.message })
-            }
-        });
+       
 
         app.get("/clientes", async (req, res) => {
             try {
-                const clientes = await ClientesRepository.buscarTodosOsClientes();
+                const clientes = await ClientesRepository.buscarTodosClientes()
                 res.status(200).json(clientes)
-            } catch (error) {
-                res.status(404).json(error.message)
+            } catch (erro) {
+                res.status(404).json(erro.message)
             }
-        });
-
+        })
 
 
         app.get("/clientes/:id", async (req, res) => {
             try {
                 const cliente = await ClientesRepository.buscarClientePorId(req.params.id)
-                if (!cliente._id) {
-                    throw new Error("Cliente não encontrado para esse id")
+                if (!cliente) {
+                    throw new Error("Id do cliente está inválido ou não cadastrado")
                 }
                 res.status(200).json(cliente)
-
-            } catch (error) {
-                res.status(404).json({ message: error.message, id: req.params.id })
+            } catch (erro) {
+                res.status(404).json({ message: erro.message, id: req.params.id })
             }
-        });
+        })
+
+        app.post("/clientes", async (req, res) => {
+            try {
+              ClientesServices.validaCamposCliente(req.body.nome, req.body.endereco, req.body.telefone, req.body.email);
+              const cliente = req.body;
+              const inserir = await ClientesRepository.criarCliente(cliente);
+              res.status(201).json(inserir);
+            } catch (erro) {
+              res.status(400).json({ message: erro.message });
+            }
+          });
 
 
         app.put("/clientes/:id", async (req, res) => {
@@ -54,28 +48,34 @@ class ClientesController {
                 const cliente = req.body
                 if (!cliente._id) {
                     throw new Error("Cliente não encontrado para esse id")
+                    console.error();
                 }
                 body.forEach((elemento) => cliente[elemento[0]] = elemento[1])
-
                 delete cliente._id
-
                 ClientesServices.validaCamposCliente(cliente.nome, cliente.pais, cliente.livros)
                 const resposta = await ClientesRepository.atualizaClientePorId(id, cliente)
-
                 res.status(200).json(resposta)
             } catch (error) {
                 res.status(400).json({ message: error.message, id })
             }
         })
+
+        // app.put("/cliente/:id", async (req, res) => {
+        //     const id = req.params.id
+		// 	const body = Object.entries(req.body)
+		// 	const valido = await  ClientesServices.validaCamposCliente(req.body.nome, req.body.endereco, req.body.telefone, req.body.email)
+		// 	if (valido) {
+		// 		await ClientesRepository.atualizarClientePorId(id, cliente)
+		// 		res.status(200).json({ message: "Cliente atualizado com sucesso" })
+		// 	} else {
+		// 		res.status(404).json({ message: "Cliente não encontrado" })
+		// 	}
+        // })
+
         
         app.delete("/clientes/:id", async (req, res) => {
             const id = req.params.id;
             try {
-                // verifica se o autor com o ID especificado existe
-                const cliente = await ClientesRepository.buscarClientePorId(id);
-                if (!cliente._id) {
-                    throw new Error("Cliente não encontrado")
-                }
                 const response = await ClientesRepository.deletarClientePorId(id)
                 res.status(200).json(response)
             } catch (error) {
